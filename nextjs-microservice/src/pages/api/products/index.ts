7f68541650498a813a90101cc19b8b4d865db1e4
bt95 <b.titus95@yahoo.com>
2025-01-07 17:23:48 +0200

    Update port & Add orderBy to GET /products

--- nextjs-microservice/src/pages/api/products/index.ts
+++ nextjs-microservice/src/pages/api/products/index.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import db from "../../../db/drizzle";
 import { getMethodNotAllowedError } from "@/utils";
 import corsMiddleware from "@/middleware/corsMiddleware";
+import { products } from "@/db/schema";
 
 export default async function productsHandler(
   req: NextApiRequest,
@@ -10,8 +11,12 @@ export default async function productsHandler(
   return corsMiddleware(req, res, async () => {
     if (req.method === "GET") {
       try {
-        const products = await db.query.products.findMany();
-        return res.status(200).json(products);
+        const productItems = await db
+          .select()
+          .from(products)
+          .orderBy(products.id)
+          .execute();
+        return res.status(200).json(productItems);
       } catch (error) {
         return res.status(500).json(new Error("Error fetching products"));
       }
