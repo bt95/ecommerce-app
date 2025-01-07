c40b4bc886f5533159a12f13c3a5822b957b54c7
bt95 <b.titus95@yahoo.com>
2025-01-07 10:57:22 +0200

    Update POST /products endpoint

--- nextjs-microservice/src/db/schema.ts
+++ nextjs-microservice/src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, varchar, integer, uuid } from "drizzle-orm/pg-core";
+import { pgTable, varchar, integer, uuid } from "drizzle-orm/pg-core";
 
 export const products = pgTable("products", {
   id: uuid("id").primaryKey().defaultRandom(),
--- nextjs-microservice/src/pages/api/products/[id].ts
+++ nextjs-microservice/src/pages/api/products/[id].ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import db from "../../../db/drizzle";
-import { InventoryItem, InventoryUpdate } from "../../../types";
+import { Product, UpdateProduct } from "../../../types";
 import { products } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
@@ -13,47 +13,76 @@ export default async function getInventoryById(
   switch (req.method) {
     case "GET": {
       try {
-        const inventoryItems: InventoryItem[] = await db
+        const productItems: Product[] = await db
           .select()
           .from(products)
           .where(eq(products.id, id))
           .limit(1)
           .execute();
 
-        if (!inventoryItems) {
-          res.status(404).json({ message: "Inventory item not found" });
+        if (!productItems?.length) {
+          return res.status(404).json(new Error("Inventory item not found"));
         }
 
-        res.status(200).json(inventoryItems[0]);
+        return res.status(200).json(productItems[0]);
       } catch (error) {
-        res.status(500).json({ message: "Internal server error", error });
+        console.log({ error });
+        return res.status(500).json(new Error("Internal server error"));
       }
       break;
     }
 
     case "POST": {
-      const updateData: InventoryUpdate = req.body;
+      const updateProductData: UpdateProduct = req.body;
       try {
-        const updatedProduct = await db
-          .insert(products)
-          .values({ ...updateData });
-        // .set({ ...updateData })
-        // .where(eq(products.id, id as string));
+        // db.execute(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME};`);
+        // db.execute(`USE ${process.env.DB_NAME};`);
+        // const result1 = await db.execute("DROP TABLE IF EXISTS products");
+        // console.log({ result1 });
+        // const result = await db.execute(
+        //   "CREATE TABLE IF NOT EXISTS products (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), name VARCHAR(255), inventory_count INT)"
+        // );
+        // console.log({ result });
 
         // const updatedProduct = await db
-        //   .update(products)
-        //   .set({ ...updateData })
-        //   .where(eq(products.id, id as string));
-        res.status(200).json(updatedProduct);
+        //   .insert(products)
+        //   .values({ ...updateData });
+        // // .set({ ...updateData })
+        // // .where(eq(products.id, id as string));
+
+        const inventoryItems: Product[] = await db
+          .select()
+          .from(products)
+          .where(eq(products.id, id))
+          .limit(1)
+          .execute();
+
+        if (!inventoryItems?.length) {
+          res.status(404).json({ message: "Inventory item not found" });
+        }
+
+        await db
+          .update(products)
+          .set({ ...updateProductData })
+          .where(eq(products.id, id as string));
+
+        const updatedProduct = await db
+          .select()
+          .from(products)
+          .where(eq(products.id, id))
+          .limit(1)
+          .execute();
+
+        return res.status(200).json(updatedProduct[0]);
       } catch (error) {
         console.log({ error });
-        res.status(500).json({ message: "Error updating product" });
+        return res.status(500).json({ message: "Error updating product" });
       }
       break;
     }
     default: {
       res.setHeader("Allow", ["GET", "POST"]);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+      return res.status(405).end(`Method ${req.method} Not Allowed`);
     }
   }
 }
